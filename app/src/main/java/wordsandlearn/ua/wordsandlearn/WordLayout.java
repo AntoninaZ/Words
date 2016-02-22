884de99d2e8c1c32d0dde910625f0be903444c1b
Antonina <zuch21@gmail.com>
2016-02-22 16:45:13 +0200

    Add word custom view

---
+++ app/src/main/java/wordsandlearn/ua/wordsandlearn/WordLayout.java
@@ -0,0 +1,17 @@
+package wordsandlearn.ua.wordsandlearn;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.RelativeLayout;
+
+/**
+ * Created by antonina on 22.02.2016.
+ */
+public class WordLayout extends RelativeLayout {
+
+    public WordLayout(Context context) {
+        super(context);
+    }
+
+
+}
