cde2ab0ef437880f7af968bb4430710c4b7f7e8e
antonina.zhuk@thinkmobiles.com <aiphei7Sieku>
2016-02-25 22:07:08 +0200

    fix sum bug

--- app/src/main/java/wordsandlearn/ua/wordsandlearn/view/UserWordLayout.java
+++ app/src/main/java/wordsandlearn/ua/wordsandlearn/view/UserWordLayout.java
@@ -14,13 +14,11 @@ import wordsandlearn.ua.wordsandlearn.WordObject;
  * Created by antonina on 25.02.2016.
  */
 public class UserWordLayout extends LinearLayout {
-    private Context mContext;
     private WordObject mWord = new WordObject();
     private int mWordLength;
 
     public UserWordLayout(Context context) {
         super(context);
-        this.mContext = context;
     }
 
     public UserWordLayout(Context context, AttributeSet attrs) {
@@ -40,28 +38,21 @@ public class UserWordLayout extends LinearLayout {
         return mWord;
      }
 
-    public void setWordLength() {
-        mWordLength  = mWord.size();
-    }
-
-    public int getmWordLength() {
-        return mWordLength;
-    }
-
     public void addLetter(LetterObject obj) {
         mWord.add(obj);
-        setWordLength();
-        TextView tvLetter = new TextView(getContext());
-        tvLetter.setTextSize(40);
-        tvLetter.setId(obj.getId());
-        tvLetter.setText(String.valueOf(obj.getLetter()));
-        this.addView(tvLetter);
+        TextView letterTV = new TextView(getContext());
+        letterTV.setTextSize(40);
+        letterTV.setId(obj.getId());
+        letterTV.setText(String.valueOf(obj.getLetter()));
+        this.addView(letterTV);
     }
 
     public void deleteLetter () {
-        LetterObject removeLetter = mWord.get(mWordLength - 1);
-        this.removeView(findViewById(removeLetter.getId()));
-        mWord.remove(mWordLength - 1);
-        setWordLength();
+        mWordLength = mWord.size();
+        if (mWordLength >= 1 ) {
+            this.removeView(findViewById(mWord.get(mWordLength - 1).getId()));
+            mWord.remove(mWordLength - 1);
+            mWordLength -= 1;
+        }
     }
 }
